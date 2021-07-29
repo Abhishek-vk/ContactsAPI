3ee973103c15995868dc61ed462fd9ace0a65ed4
Abhishek <abhishek.vk100@gmail.com>
2021-07-29 21:19:55 +0530

    Get method added

--- App.js
+++ App.js
@@ -1,28 +1,33 @@
-var express = require('express');
-var cors = require('cors');
-var fs = require('fs');
+var express = require("express");
+var cors = require("cors");
+var fs = require("fs");
 
-var app=express();
+var app = express();
 app.use(cors());
 app.use(express.json());
 
-const port=8000;
-const urlPath='/api/v1/contacts';
-var data=JSON.parse(fs.readFileSync('./data/Contacts.json'));
+const port = 8000;
+const urlPath = "/api/v1/contacts";
+var data = JSON.parse(fs.readFileSync("./data/Contacts.json"));
 
-app.get(urlPath,(req,res)=>{
+app.get(urlPath, (req, res) => {
     res.status(200).json({
-        status:'Success',
-        statusCode:200,
-        total:data.length,
-        data});
+        status: "Success",
+        statusCode: 200,
+        total: data.length,
+        data,
+    });
 });
 
-app.post(urlPath,(req,res)=>{
-    res.status(200).json({status:'Success',statusCode:200,addedData:req.body});
-})
+app.post(urlPath, (req, res) => {
+    res.status(200).json({
+        status: "Success",
+        statusCode: 200,
+        addedData: req.body,
+    });
+});
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.clear();
-    console.log('Server started at port 8000');
+    console.log("Server started at port 8000");
 });
